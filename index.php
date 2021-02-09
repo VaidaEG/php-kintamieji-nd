ed3270c42ef82506de0cdb0f54cbfc884ba09224
VaidaEG <grausliene.vaida@gmail.com>
2021-02-09 23:33:35 +0200

    9-10 uzduotys

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-1.
+<h1>1.</h1>
 <?php
 $name = 'Vaida';
 $surname = 'Eidžiulytė-Grauslienė';
@@ -6,10 +6,9 @@ $birthYear = 1989;
 $currentYear = 2021;
 $age = $currentYear - $birthYear;
 echo " Aš esu $name $surname. Man yra $age metai(ų)."
-
 ?>
 <br>
-2.
+<h1>2.</h1>
 <?php
 $randomFirst = rand(0, 4);
 $randomSecond = rand(0, 4);
@@ -20,7 +19,7 @@ if ($randomFirst >= $randomSecond && $randomSecond !== 0) {
 }
 ?>
 <br>
-3.
+<h1>3.</h1>
 <?php
 $randomFirst = rand(0, 25);
 $randomSecond = rand(0, 25);
@@ -34,7 +33,7 @@ if ($randomSecond < $randomFirst && $randomFirst < $randomThird || $randomThird 
 }
 ?>
 <br>
-4.
+<h1>4.</h1>
 <?php
 $a = rand(1, 10);
 $b = rand(1, 10);
@@ -46,7 +45,7 @@ if (($a + $b) > $c && ($a + $c) > $b && ($b + $c) > $a) {
     echo 'No. Sides a, b and c do not form triangle. <br>';
 }
 ?>
-5.
+<h1>5.</h1>
 <?php
 $number1 = rand(0, 2);
 $number2 = rand(0, 2);
@@ -94,40 +93,40 @@ if ($number5 == 0) {
 echo "There are $zero zeros, $one ones and $two twos.";
 ?>
 <br>
-6.
+<h1>6.</h1>
 <?php
 $randomNumber = rand(1, 6);
-echo "<h1>" . $randomNumber . "</h1>";
+echo "<h2>" . $randomNumber . "</h2>";
 ?>
-7.
+<h1>7.</h1>
 <?php
 $number1 = rand(-10, 10);
 $number2 = rand(-10, 10);
 $number3 = rand(-10, 10);
 if ($number1 < 0) {
-    echo "<h1 style=color:green;>" . $number1 . "</h1>";
+    echo "<h2 style=color:green;>" . $number1 . "</h2>";
 } else if ($number1 == 0) {
-    echo "<h1 style=color:red;>" . $number1 . "</h1>";
+    echo "<h2 style=color:red;>" . $number1 . "</h2>";
 } else {
-    echo "<h1 style=color:blue;>" . $number1 . "</h1>";
+    echo "<h2 style=color:blue;>" . $number1 . "</h2>";
 }
 if ($number2 < 0) {
-    echo "<h1 style=color:green;>" . $number2 . "</h1>";
+    echo "<h2 style=color:green;>" . $number2 . "</h2>";
 } else if ($number2 == 0) {
-    echo "<h1 style=color:red;>" . $number2 . "</h1>";
+    echo "<h2 style=color:red;>" . $number2 . "</h2>";
 } else {
-    echo "<h1 style=color:blue;>" . $number2 . "</h1>";
+    echo "<h2 style=color:blue;>" . $number2 . "</h2>";
 }
 if ($number3 < 0) {
-    echo "<h1 style=color:green;>" . $number3 . "</h1>";
+    echo "<h2 style=color:green;>" . $number3 . "</h2>";
 } else if ($number3 == 0) {
-    echo "<h1 style=color:red;>" . $number3 . "</h1>";
+    echo "<h2 style=color:red;>" . $number3 . "</h2>";
 } else {
-    echo "<h1 style=color:blue;>" . $number3 . "</h1>";
+    echo "<h2 style=color:blue;>" . $number3 . "</h2>";
 }
 ?>
 <br>
-8.
+<h1>8.</h1>
 <?php
 $price = 1.0;
 $candleQuantity = rand(5, 3000);
@@ -143,3 +142,56 @@ if ($sum >= 1000 && $sum < 2000) {
 }
 ?>
 <br>
+<h1>9.</h1>
+<?php
+$numberFirst = rand(0, 100);
+$numberSecond = rand(0, 100);
+$numberThird = rand(0, 100);
+$sum = 0;
+$quantity = 0;
+if ($numberFirst > 10 && $numberFirst < 90) {
+    $sum += $numberFirst;
+    $quantity++;
+}
+if ($numberSecond > 10 && $numberSecond < 90) {
+    $sum += $numberSecond;
+    $quantity++;
+}
+if ($numberThird > 10 && $numberThird < 90) {
+    $sum += $numberThird;
+    $quantity++;
+}
+$average = $sum / $quantity;
+$roundAverage = round($average, 0);
+echo "$roundAverage";
+?>
+<br>
+<h1>10.</h1>
+<?php
+$hours = rand(0, 23);
+$minutes = rand(0, 59);
+$seconds = rand(0, 59);
+$extraSeconds = rand(0, 300);
+echo "$hours hours $minutes minutes $seconds seconds <br>";
+$allSeconds = $seconds + $extraSeconds;
+echo "Extra seconds - $extraSeconds <br>";
+if ($allSeconds >= 60) {
+    $extraMinutes = intval($allSeconds / 60);
+    $seconds = $allSeconds - $extraMinutes * 60;
+    $minutes += $extraMinutes; 
+} else {
+    $seconds = $allSeconds;
+    $minutes = $minutes;  
+}
+if ($minutes >= 60) {
+    $hours += intval($minutes / 60);
+    if ($hours === 24) {
+        $hours = 0;
+    }
+    $minutes -= $hours * 60; 
+} 
+
+echo "$hours hours $minutes minutes $seconds seconds <br>";
+?>
+
+
